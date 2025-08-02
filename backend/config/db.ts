15bd78c554b02642c2ec349b84a1928f31f514a0
fawkesdbs <s.mazibuko1108@gmail.com>
2025-08-02 06:04:20 +0200

    Seeding data

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,12 +2,12 @@ import { createClient } from "@supabase/supabase-js";
 import "dotenv/config";
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE;
 
 console.log("Supabase URL:", supabaseUrl);
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase URL and anon key are required.");
+if (!supabaseUrl || !supabaseServiceKey) {
+  throw new Error("Supabase URL and service key are required.");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
